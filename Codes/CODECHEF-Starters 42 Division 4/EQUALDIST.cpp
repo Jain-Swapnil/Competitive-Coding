963309e62301ea4513870de394fc9ea2ad338715
Swapnil Jain <76614640+Jain-Swapnil@users.noreply.github.com>
2022-06-09 17:54:24 +0530

    Create EQUALDIST.cpp

---
+++ Codes/CODECHEF-Starters 42 Division 4/EQUALDIST.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+		int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cin>>a;
+	    cin>>b;
+	    if((a+b)%2==0)
+	     {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	
+	return 0;
+}

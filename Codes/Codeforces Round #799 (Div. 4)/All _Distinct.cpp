f18ac20b7dc17636e09050836fb8c75f69856921
Swapnil Jain <76614640+Jain-Swapnil@users.noreply.github.com>
2022-06-15 10:05:06 +0530

    Create All _Distinct.cpp

---
+++ Codes/Codeforces Round #799 (Div. 4)/All _Distinct.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int uniqueElements(int a[], int size){
+   int i, j, count = 1;
+   for (i = 1; i < size; i++){
+      for (j = 0; j < i; j++){
+         if (a[i] == a[j]){
+            break;
+         }
+      }
+      if (i == j){
+         count++;
+      }
+   }
+   return count;
+}
+int main()
+{
+    int t,count=0;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        if(n%2==0)
+        {
+            int ans=uniqueElements(arr,n);
+           
+            if(ans%2==0)
+            {
+                cout<<ans<<endl;
+            }
+            else
+            {
+              cout<<ans-1<<endl;  
+            }
+            
+        }
+        else
+        {
+            
+            int ans=uniqueElements(arr,n);
+            if(ans%2==0)
+            {
+                cout<<ans-1<<endl;
+            }
+            else
+            {
+              cout<<ans<<endl;  
+            }
+            
+        }
+    }
+
+    return 0;
+}

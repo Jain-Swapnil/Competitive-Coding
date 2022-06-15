8fb3e2e5e5c1c8ee04c28e19404c4b3b210f1d1c
Swapnil Jain <76614640+Jain-Swapnil@users.noreply.github.com>
2022-06-15 10:04:13 +0530

    Create Marathon.cpp

---
+++ Codes/Codeforces Round #799 (Div. 4)/Marathon.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        int arr[4];
+        int n=4;
+        for(int i=0;i<4;i++)
+        {
+        cin>>arr[i];
+        }
+		a=arr[0];
+        sort(arr, arr + n);
+        for(int i=0;i<4;i++)
+        {
+			
+            if(arr[i]==a)
+            {
+				int a=4-(i+1);
+                cout<<a%4<<endl;
+            }
+        }
+    }
+
+    return 0;
+}

81f7652527084beb96c6f1e26c33b102139fc0c7
Swapnil Jain <76614640+Jain-Swapnil@users.noreply.github.com>
2022-06-30 00:34:57 +0530

    Create PRIZEPOOL.cpp

---
+++ Codes/Practice-Codechef/PRIZEPOOL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,t;
+	cin>>t;
+	for(int i=1; i<=t;i++)
+	{
+	    cin>>a;
+	    cin>>b;
+	    cout<< 10*a+90*b<<endl;
+	}
+	return 0;
+}

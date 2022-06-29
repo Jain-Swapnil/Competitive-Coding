c3a3b7d88a28590f3ff2e31a940d41305702ae17
Swapnil Jain <76614640+Jain-Swapnil@users.noreply.github.com>
2022-06-30 00:25:38 +0530

    Update CHEFAPPS.cpp

--- Codes/Codechef -Starters 45 Division 4 (Rated)/CHEFAPPS.cpp
+++ Codes/Codechef -Starters 45 Division 4 (Rated)/CHEFAPPS.cpp
@@ -7,15 +7,36 @@ int main() {
 	cin>>t;
 	while(t--)
 	{
-	    int a,b,c,d;
+	    int a,b,c,d,n;
 	    cin>>a>>b>>c>>d;
-	    if(a==c || b==d)
-	   {
-	       cout<<2<<endl;
-	   } 
-	   else{
-	       cout<<1<<endl;
-	   }
+	    if((b+c+d)<=a)
+	    {
+	        cout<<0<<endl;
+	    }
+	    else{
+	        int n=b+c;
+	        int p=a-b-c;
+	        if(b>c)
+	        {
+	            if((p+b-d)<=a && (p+b-d)>=0)
+	            {
+	                cout<<1<<endl;
+	            }
+	            else if((p+b+c-d)<=a && (p+b+c-d)>0){
+	                cout<<2<<endl;
+	            }
+	        }
+	        else
+	        {
+	            if(p+c-d<=a && p+c-d>=0)
+	            {
+	                cout<<1<<endl;
+	            }
+	             else if((p+b+c-d)<=a && (p+b+c-d)>=0){
+	                cout<<2<<endl;
+	            }
+	        }
+	    }
 	}
 	return 0;
 }

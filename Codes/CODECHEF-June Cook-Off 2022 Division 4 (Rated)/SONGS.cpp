210905d253660855d92c0af66539f6767318234b
Swapnil Jain <76614640+Jain-Swapnil@users.noreply.github.com>
2022-07-08 10:26:32 +0530

    Create SONGS.cpp

---
+++ Codes/CODECHEF-June Cook-Off 2022 Division 4 (Rated)/SONGS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,n;
+	    cin>>n;
+	    cin>>x;
+	    int ans=0;
+	    ans= n/(3*x);
+	    cout<<ans<<endl;
+	    
+	}
+	return 0;
+}

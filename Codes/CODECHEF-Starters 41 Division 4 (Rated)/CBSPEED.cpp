cdace85c9c2527f94c751f0da8d30e7aab57ed6d
Swapnil Jain <76614640+Jain-Swapnil@users.noreply.github.com>
2022-07-08 10:20:07 +0530

    Create CBSPEED.cpp

---
+++ Codes/CODECHEF-Starters 41 Division 4 (Rated)/CBSPEED.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long int x,y;
+	cin>>x;
+	cin>>y;
+	if(y>x)
+	{
+	    cout<<"YES";
+	}
+	else{
+	    cout<<"NO";
+	}
+	return 0;
+}
